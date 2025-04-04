105a26d5d9c92ea1c7a0fa64d749911ad4dfb12e
ema-p460 <pemasugga@gmail.com>
2025-04-04 10:46:00 +0800

    "this is worksheet2session3"

---
+++ worksheet2session3&4.py
@@ -0,0 +1,19 @@
+# List of departments with course_id and department
+courses = [
+    [1, "Computer Science"],
+    [2, "Mathematics"],
+    [3, "Computer Science"],
+    [4, "Mathematics"],
+    [5, "Physics"],
+    [6, "Chemistry"],
+    [7, "Biology"],
+    [8, "Economics"],
+    [9, "Economics"],
+    [10, "Psychology"],
+    [11, "History"],
+    [12, "English"],
+    [13, "Philosophy"],
+    [14, "Mathematics"],
+    [15, "Computer Science"]
+]
+print (courses)
