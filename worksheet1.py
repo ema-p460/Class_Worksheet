453a89ad0131b6035b269bf41a116f67bcd9a3af
ema-p460 <pemasugga@gmail.com>
2025-04-04 10:19:08 +0800

    this is worksheet 1

---
+++ worksheet1.py
@@ -0,0 +1,40 @@
+a=15# work 1
+b=12
+type(a)
+type(b)
+print(type(a))
+print(type(a)) 
+
+#work 2
+addition=a+b
+subtration=a-b
+multiply=a*b
+division=a/b
+
+print("a+b=",addition )
+print("a-b=",subtration)
+print("a*b=",multiply)
+print("a/b=",division)
+
+#work 3
+c=a//b
+c=int(c)
+type(int(c))
+print("c=",c)
+print(type(int(c)))
+
+
+c=float(c)
+type(float(c))
+print("new c=",c)
+print("new type=",type(float(c)))
+
+#work 4
+fstring=f"The result of a divided by b is:{str(c)}" 
+print(fstring)
+
+#work 5
+
+
+print("a>b=",a>b)
+print("a==b",a==b)
